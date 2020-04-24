2aaedcfa89546a2f985a4f86c152fd417655b418
zetareticoli <zetareticoli@raven.lan>
2020-04-24 14:23:59 +0200

    Changed imagemin path

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
  
 exports.default = () => (
-    gulp.src('img/posts/*')
+    gulp.src('img/posts/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('_site/img/posts/'))
 );
