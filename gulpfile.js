5379d3fc1d47489505f918b13d33b3cdec76083e
zetareticoli <zetareticoli@raven.lan>
2020-04-28 17:30:08 +0200

    Imagemin optimization

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
  
 exports.default = () => (
-    gulp.src('img/posts/**/*')
+    gulp.src('img/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('_site/img/posts/'))
+        .pipe(gulp.dest('img/'))
 );
